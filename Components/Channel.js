b8772720f11040d01df2b6dc53a7af4805dbdd9e
Berke Gençkaya <berkegenckaya@gmail.com>
2022-12-16 17:31:12 +0300

    Update Channel.js

--- Components/Channel.js
+++ Components/Channel.js
@@ -8,24 +8,25 @@ const Channel = () => {
           width: "100%",
           display: "flex",
           justifyContent: "space-between",
-          
         }}
       >
-        <div style={{ display: "flex", flexDirection: "column" , justifyContent:"space-between"}}>
-         
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
           <div className={styles.channeltitle}>GENERAL DISCUSSION</div>{" "}
-          
           <div>
-          <div>
-            <img src="./assets/i/image.png"></img>
-          </div>
-        
+            <div>
+              <img src="./assets/i/image.png"></img>
+            </div>
             <div className={styles.channelsubtitle}>
               Share stories, ideas, memes and more!
             </div>
-         </div>
+          </div>
         </div>
-    
       </div>
     </div>
   );
