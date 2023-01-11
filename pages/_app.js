574deb1b3a061dce483a420b7afff659b3958977
Berke Gençkaya <berkegenckaya@gmail.com>
2023-01-11 16:24:34 +0300

    font link

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import { ChainId, ThirdwebProvider } from '@thirdweb-dev/react';
-import Link from 'next/link';
+
 import "../styles/globals.css";
 
 // This is the chainId your dApp will work on.
@@ -8,8 +8,8 @@ const activeChainId = ChainId.Mainnet;
 function MyApp({ Component, pageProps }) {
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <Link href="/">
-      <Component {...pageProps} /></Link>
+    
+      <Component {...pageProps} />
     </ThirdwebProvider>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,17 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
+    <Link rel="preload" href="./public/assets/fonts/Jost-SemiBold.ttf">
     <div className={styles.container}>
       <main className={styles.main}  style={{ overflow: "hidden"}}>
         <Header></Header>
         <div style={{ display: "flex" }}>
           <Menu></Menu>
+          
           <div>HTG</div>
         </div>
        
       </main>
-    </div>
+    </div></Link>
   );
 }
